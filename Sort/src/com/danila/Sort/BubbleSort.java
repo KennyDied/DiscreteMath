2ec18d2d77908938c780e375d78050ab89095832
KennyDied <thekennydied@mail.ru>
2020-11-11 16:00:09 +0300

    add ShellSort and BubbleSort

--- Sort/src/com/danila/Sort/BubbleSort.java
+++ Sort/src/com/danila/Sort/BubbleSort.java
@@ -1,4 +1,34 @@
 package com.danila.Sort;
 
 public class BubbleSort {
+    public static void sort(int[] array) {
+        System.out.println("Массив до сортировки: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        int swaps = 0;
+        int compares = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                compares++;
+                if (array[j] > array[j + 1]) {
+                    swaps++;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Массив после сортировки: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Было проведено " + (swaps) + " обменов.");
+        System.out.println("Было проведено " + (compares) + " сравнений.");
+    }
 }
--- Sort/src/com/danila/Sort/ShellSort.java
+++ Sort/src/com/danila/Sort/ShellSort.java
@@ -1,4 +1,36 @@
 package com.danila.Sort;
 
 public class ShellSort {
+    public static void sort(int[] array) {
+        System.out.println("Массив до сортировки: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        int swaps = 0;
+        int compares = 0;
+        int step = array.length;       //изначатьный шаг равен половине длины массива
+
+        for (int gap = step / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < step; i++) {
+                int key = array[i];
+                int j = i;
+                while (j >= gap && array[j - gap] > key) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = key;
+            }
+        }
+
+        System.out.println("Массив после сортировки: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println("Было проведено " + (swaps) + " обменов.");
+        System.out.println("Было проведено " + (compares) + " сравнений.");
+    }
 }
