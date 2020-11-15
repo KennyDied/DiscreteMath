04073ba22f2d1fbded1ba7bf7f7d9e92165c227a
KennyDied <thekennydied@mail.ru>
2020-11-15 19:45:34 +0300

    edit code

--- Sort/src/com/danila/Sort/ShellSort.java
+++ Sort/src/com/danila/Sort/ShellSort.java
@@ -8,29 +8,44 @@ public class ShellSort {
         }
         System.out.println();
 
-        int swaps = 0;
-        int compares = 0;
-        int step = array.length;       //изначатьный шаг равен половине длины массива
+//        int swaps = 0;
+//        int compares = 0;
+//        int step = array.length;       //изначатьный шаг равен половине длины массива
+//        for (int gap = step / 2; gap > 0; gap /= 2) {
+//            for (int i = gap; i < step; i++) {
+//                int key = array[i];
+//                int j = i;
+//                while (j >= gap && array[j - gap] > key) {
+//                    array[j] = array[j - gap];
+//                    j -= gap;
+//                }
+//                array[j] = key;
+//            }
+//        }
 
-        for (int gap = step / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < step; i++) {
-                int key = array[i];
-                int j = i;
-                while (j >= gap && array[j - gap] > key) {
-                    array[j] = array[j - gap];
-                    j -= gap;
+        int n = array.length;
+        int counter = 0;
+        for (int step = n / 2; step > 0; step /= 2) {
+            for (int i = step; i < n; i++) {
+                for (int j = i - step; j >= 0 ; j -= step) {
+                    counter++;
+                    int x = array[j];
+                    array[j] = array[j + step];
+                    array[j + step] = x;
                 }
-                array[j] = key;
             }
         }
+        System.out.println("Было проведено " + (counter) + " операций.");
+
+
 
         System.out.println("Массив после сортировки: ");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
 
-        System.out.println();
-        System.out.println("Было проведено " + (swaps) + " обменов.");
-        System.out.println("Было проведено " + (compares) + " сравнений.");
+//        System.out.println();
+//        System.out.println("Было проведено " + (swaps) + " обменов.");
+//        System.out.println("Было проведено " + (compares) + " сравнений.");
     }
 }
